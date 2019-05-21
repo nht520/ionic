58430435e9b7db9437cc2a0e1e4900309ad85e62
NHT <wlgou6363@qq.com>
2019-05-21 21:12:58 +0800

    ionic

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+// ionic 打包app以后配置启动画面以及导航条的服务
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+// 引入路由配置文件
+import { AppRoutingModule } from './app-routing.module';
+// 引入根组件
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent], /*引入根组件 */
+  entryComponents: [], // 配置不会在模版中使用的组件
+  imports: [ // 映入模块 和自定义模块
+    BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [ //配置服务
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
